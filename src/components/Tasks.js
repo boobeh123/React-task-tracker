9d17faffd86074df816021352148e086cca8b926
Boobeh123 <boobeh321@gmail.com>
2022-12-08 20:28:37 -1000

    Initial commit

---
+++ src/components/Tasks.js
@@ -0,0 +1,16 @@
+import Task from './Task'
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+    {tasks.map((element) => (
+    <Task key={element.id}
+    task={element} 
+    onDelete={onDelete}
+    onToggle={onToggle} />
+    ))}
+    </>
+  )
+}
+
+export default Tasks
